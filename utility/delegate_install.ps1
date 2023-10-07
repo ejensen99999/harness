e414ce1cbd3fca0bd9cfc221d0d9fc9c3afc5f96
Eric Jensen <ejensen99999@comcast.net>
2023-10-07 09:59:45 -0600

    test

--- utility/delegate_install.ps1
+++ utility/delegate_install.ps1
@@ -5,4 +5,4 @@ docker run --cpus=1 --memory=2g `
   -e ACCOUNT_ID=fQlxjFqlTgqv_gPDCPsIcA `
   -e DELEGATE_TOKEN=MzNlZGVhYzAzNmI0YzkyMTFmMmY1OTA5OTZiOGUwNTY= `
   -e LOG_STREAMING_SERVICE_URL=https://app.harness.io/log-service/ `
-  -e MANAGER_HOST_AND_PORT=https://app.harness.io harness/delegate:23.09.80505
+  -e MANAGER_HOST_AND_PORT=https://app.harness.io ejensen99999/base-delegate:full
