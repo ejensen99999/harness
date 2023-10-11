625648b8667a905177fd1b36c6a28cbeab644157
Eric Jensen <ejensen99999@comcast.net>
2023-10-11 14:28:29 -0600

    refactor to remove craft and redo packer config

--- image/linux/support/dependencies.sh
+++ image/linux/support/dependencies.sh
@@ -8,32 +8,16 @@ sudo apt-get install -y \
     ansible \
     ca-certificates \
     curl \
-    kubectl \
-    software-properties-common \
-    packer=1.7.8
-
-pip install requests-credssp
-
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-sudo chmod a+x /usr/local/bin/yq        
-
-wget https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz
-tar xf kubeval-linux-amd64.tar.gz
-sudo cp kubeval /usr/local/bin
-
-wget -q https://github.com/Azure/kubelogin/releases/download/v0.0.20/kubelogin-linux-amd64.zip
-unzip kubelogin-linux-amd64.zip
-sudo cp bin/linux_amd64/* /usr/local/bin
-
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+    software-properties-common
 
 # echo "Running as user:"
 # whoami
 
 # echo "Where is ansible-playbook?"
 # which ansible-playbook
+user=$(whoami)
 
-sudo chmod 777 -R /home/runner/.ansible
+sudo chmod 777 -R /home/$user/.ansible
 
 # echo "What version is it?"
 # ansible-playbook --version
