f2e4818ec26d641811e1b485012e5c63518b983e
Eric Jensen <ejensen99999@comcast.net>
2023-10-13 11:52:30 -0600

    testing container

--- utility/delegate_install.sh
+++ utility/delegate_install.sh
@@ -1,8 +1,15 @@
+#!/usr/bin/env bash
+
+echo "here"
+
 docker run --cpus=1 --memory=2g \
-  -e DELEGATE_NAME=docker-delegate \
+  -e DELEGATE_NAME=custom-delegate \
   -e NEXT_GEN="true" \
   -e DELEGATE_TYPE="DOCKER" \
   -e ACCOUNT_ID=fQlxjFqlTgqv_gPDCPsIcA \
   -e DELEGATE_TOKEN=MzNlZGVhYzAzNmI0YzkyMTFmMmY1OTA5OTZiOGUwNTY= \
   -e LOG_STREAMING_SERVICE_URL=https://app.harness.io/log-service/ \
-  -e MANAGER_HOST_AND_PORT=https://app.harness.io ejensen99999/custom-job-delegate:harness
+  -e MANAGER_HOST_AND_PORT=https://app.harness.io \
+  ejensen99999/custom-job-delegate:harness
+
+  echo "there"
